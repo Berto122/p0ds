00bcc2ff9d8c4e6f8c2e55850040f5f3f2335a69
Victor B. <166399656+Berto122@users.noreply.github.com>
2025-04-30 08:20:17 -0300

    Update trabalho.sql

--- trabalho.sql
+++ trabalho.sql
@@ -4,3 +4,5 @@ CREATE TABLE clientes_online(
   email VARCHAR(100)
   cidade VARCHAR(100)
 )
+
+INSERT INTO clientes_online (id_cliente, nome, email, cidade)VALUES(1, 'ana silva','ana@email.com','sao paulo'),
